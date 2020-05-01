22c86698066b7a24ba0293c25df6d6d21e6f75ba
Sai rohith gorla <sairohithgorla01@gmail.com>
2020-05-01 14:51:00 -0500

    COMMIT

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-let createError = require('http-errors');
-const dotenv = require('dotenv')
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const dotenv = require('dotenv');
+var compression = require('compression');
+var helmet = require('helmet');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog'); 
 
-let app = express();
+var app = express();
+dotenv.config({ path: '.env' })
 
-//Set up mongoose connection
-let mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://Sai:<Sai>@cluster0-gdkck.mongodb.net/Sai?retryWrites=true&w=majority';
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = process.env.ATLAS_URI;
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true });
-const db = mongoose.connection;
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-dotenv.config({ path: '.env' })
 // view engine setup
-dev_db_url = process.env.ATLAS_URI
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
